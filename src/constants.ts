708473fa65b03792fbf3027a8a3fc4c1a6c4a850
d1ll0n <root@pc>
2023-07-19 18:03:35 -0700

    v1.0.5

--- src/constants.ts
+++ src/constants.ts
@@ -11,7 +11,7 @@ import { SignerOrProvider } from "./types";
 
 export const LensAddress = "0xDdcBBC4510156441F88D23A6ab4c64eed1775C16";
 export const FactoryAddress = "0x2050084919527BDE33Ff2dc9ef3768B2E335AC71";
-export const ControllerAddress = "0x41176b30395e1eea87dd6deb6d796c56097f6fa0";
+export const ControllerAddress = "0x41176b30395e1EeA87dd6deb6d796c56097F6fA0";
 
 export const getControllerContract = (provider: SignerOrProvider): WildcatVaultController => {
   return WildcatVaultController__factory.connect(ControllerAddress, provider);
@@ -26,4 +26,5 @@ export const getLensContract = (provider: SignerOrProvider): VaultLens => {
 };
 
 export const RAY = BigNumber.from(10).pow(27);
+
 export const WAD = BigNumber.from(10).pow(18);
