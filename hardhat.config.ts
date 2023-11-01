7cbd9c33431b78f46ad2f70af2aaaadcc2af1103
d1ll0n <dillon@indexed.finance>
2023-11-01 07:52:28 -0500

    update hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomicfoundation/hardhat-chai-matchers";
-import 'dotenv/config'
+import "dotenv/config";
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -11,18 +11,22 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.17",
-      },
-    ],
+        version: "0.8.20"
+      }
+    ]
   },
   networks: {
     hardhat: {
-      chainId: 11155111,      
+      chainId: 11155111,
       forking: {
-        url: 'https://eth-sepolia.g.alchemy.com/v2/Dwn5l0Y-DcxyShxHVAqreAUkULi4eznO',
-        blockNumber: 3654504
+        url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
       }
     }
+  },
+  typechain: {
+    discriminateTypes: true,
+    outDir: "typechain",
+    target: "ethers-v5"
   }
 };
 
