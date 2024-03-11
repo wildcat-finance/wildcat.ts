5247dcfce4d97cc812bedc273503895a13b95e91
d1ll0n <dillon@indexed.finance>
2024-03-11 03:59:00 -0700

    Update market record types

--- src/utils/type-parsers.ts
+++ src/utils/type-parsers.ts
@@ -3,9 +3,13 @@ import { PartialTransaction, SignerOrProvider } from "../types";
 import { BigNumber, PopulatedTransaction, constants } from "ethers";
 import { Token, TokenAmount } from "../token";
 import {
+  SubgraphAnnualInterestBipsUpdatedDataFragment,
   SubgraphBorrowDataFragment,
+  SubgraphDelinquencyStatusChangedDataFragment,
   SubgraphDepositDataFragment,
   SubgraphFeesCollectedDataFragment,
+  SubgraphMarketClosedDataFragment,
+  SubgraphMaxTotalSupplyUpdatedDataFragment,
   SubgraphRepaymentDataFragment,
   SubgraphWithdrawalBatchPaymentPropertiesFragment,
   SubgraphWithdrawalExecutionPropertiesFragment,
@@ -60,13 +64,10 @@ export const parseFeeConfiguration = (
   };
 };
 
-// export type WithdrawalRequestRecord = {
-//   normalizedAmount: TokenAmount;
-// } & Omit<SubgraphWithdrawalRequestPropertiesFragment, "normalizedAmount">;
-
 export class WithdrawalRequestRecord {
   constructor(
     public id: string,
+    public eventIndex: number,
     public requestIndex: number,
     public scaledAmount: BigNumber,
     public normalizedAmount: TokenAmount,
@@ -104,6 +105,7 @@ export class WithdrawalRequestRecord {
   ): WithdrawalRequestRecord {
     return new WithdrawalRequestRecord(
       data.id,
+      data.eventIndex,
       data.requestIndex,
       BigNumber.from(data.scaledAmount),
       batch.market.underlyingToken.getAmount(data.normalizedAmount),
@@ -174,39 +176,181 @@ export const removeUnusedTxFields = ({
   };
 };
 
+export type MaxTotalSupplyUpdatedRecord = Omit<
+  SubgraphMaxTotalSupplyUpdatedDataFragment,
+  "oldMaxTotalSupply" | "newMaxTotalSupply" | "__typename"
+> & {
+  __typename: "MaxTotalSupplyUpdated";
+  oldMaxTotalSupply: TokenAmount;
+  newMaxTotalSupply: TokenAmount;
+};
+
+export type AnnualInterestBipsUpdatedRecord = Omit<
+  SubgraphAnnualInterestBipsUpdatedDataFragment,
+  "__typename"
+> & {
+  __typename: "AnnualInterestBipsUpdated";
+};
+
+export type DelinquencyStatusChangedRecord = Omit<
+  SubgraphDelinquencyStatusChangedDataFragment,
+  "liquidityCoverageRequired" | "totalAssets" | "__typename"
+> & {
+  __typename: "DelinquencyStatusChanged";
+  liquidityCoverageRequired: TokenAmount;
+  totalAssets: TokenAmount;
+};
+
+export type MarketClosedRecord = Omit<SubgraphMarketClosedDataFragment, "__typename"> & {
+  __typename: "MarketClosed";
+};
+
 export type DepositRecord = {
+  __typename: "Deposit";
   amount: TokenAmount;
   address: string;
-} & Omit<SubgraphDepositDataFragment, "assetAmount" | "account">;
+} & Omit<SubgraphDepositDataFragment, "assetAmount" | "account" | "__typename">;
 
 export type RepaymentRecord = {
+  __typename: "DebtRepaid";
   amount: TokenAmount;
-} & Omit<SubgraphRepaymentDataFragment, "assetAmount">;
+} & Omit<SubgraphRepaymentDataFragment, "assetAmount" | "__typename">;
 
 export type BorrowRecord = {
+  __typename: "Borrow";
   amount: TokenAmount;
-} & Omit<SubgraphBorrowDataFragment, "assetAmount">;
+} & Omit<SubgraphBorrowDataFragment, "assetAmount" | "__typename">;
 
 export type FeeCollectionRecord = {
+  __typename: "FeesCollected";
   amount: TokenAmount;
-} & Omit<SubgraphFeesCollectedDataFragment, "feesCollected">;
+} & Omit<SubgraphFeesCollectedDataFragment, "feesCollected" | "__typename">;
 
+export type WithdrawalRequestPartialRecord = Omit<
+  SubgraphWithdrawalRequestPropertiesFragment,
+  "scaledAmount" | "normalizedAmount" | "__typename"
+> & {
+  __typename: "WithdrawalRequest";
+  scaledAmount: BigNumber;
+  normalizedAmount: TokenAmount;
+};
+
+export type MarketRecord =
+  | AnnualInterestBipsUpdatedRecord
+  | BorrowRecord
+  | DelinquencyStatusChangedRecord
+  | DepositRecord
+  | FeeCollectionRecord
+  | MarketClosedRecord
+  | MaxTotalSupplyUpdatedRecord
+  | RepaymentRecord
+  | WithdrawalRequestPartialRecord;
+
+export type MarketDataFragment =
+  | SubgraphAnnualInterestBipsUpdatedDataFragment
+  | SubgraphBorrowDataFragment
+  | SubgraphDelinquencyStatusChangedDataFragment
+  | SubgraphDepositDataFragment
+  | SubgraphFeesCollectedDataFragment
+  | SubgraphMarketClosedDataFragment
+  | SubgraphMaxTotalSupplyUpdatedDataFragment
+  | SubgraphRepaymentDataFragment
+  | SubgraphWithdrawalRequestPropertiesFragment;
+
+const isDeposit = (x: any): x is SubgraphDepositDataFragment => x.__typename === "Deposit";
+const isDelinquencyStatusChanged = (x: any): x is SubgraphDelinquencyStatusChangedDataFragment =>
+  x.__typename === "DelinquencyStatusChanged";
+const isMarketClosed = (x: any): x is SubgraphMarketClosedDataFragment =>
+  x.__typename === "MarketClosed";
+
+const isAnnualInterestBipsUpdated = (x: any): x is SubgraphAnnualInterestBipsUpdatedDataFragment =>
+  x.__typename === "AnnualInterestBipsUpdated";
+
+const isMaxTotalSupplyUpdated = (x: any): x is SubgraphMaxTotalSupplyUpdatedDataFragment =>
+  x.__typename === "MaxTotalSupplyUpdated";
+const isFeesCollected = (x: any): x is SubgraphFeesCollectedDataFragment =>
+  x.__typename === "FeesCollected";
+const isWithdrawalRequest = (x: any): x is SubgraphWithdrawalRequestPropertiesFragment =>
+  x.__typename === "WithdrawalRequest";
+
+const hasTypeName = <T extends MarketDataFragment>(
+  x: T
+): x is T & { __typename: Exclude<T["__typename"], undefined> } => x.__typename !== undefined;
+
+export function parseMarketRecord(
+  token: Token,
+  log: SubgraphAnnualInterestBipsUpdatedDataFragment
+): AnnualInterestBipsUpdatedRecord;
+export function parseMarketRecord(token: Token, log: SubgraphBorrowDataFragment): BorrowRecord;
+export function parseMarketRecord(
+  token: Token,
+  log: SubgraphDelinquencyStatusChangedDataFragment
+): DelinquencyStatusChangedRecord;
 export function parseMarketRecord(token: Token, log: SubgraphDepositDataFragment): DepositRecord;
+export function parseMarketRecord(
+  token: Token,
+  log: SubgraphFeesCollectedDataFragment
+): FeeCollectionRecord;
+export function parseMarketRecord(
+  token: Token,
+  log: SubgraphMarketClosedDataFragment
+): MarketClosedRecord;
+export function parseMarketRecord(
+  token: Token,
+  log: SubgraphMaxTotalSupplyUpdatedDataFragment
+): MaxTotalSupplyUpdatedRecord;
 export function parseMarketRecord(
   token: Token,
   log: SubgraphRepaymentDataFragment
 ): RepaymentRecord;
-export function parseMarketRecord(token: Token, log: SubgraphBorrowDataFragment): BorrowRecord;
 export function parseMarketRecord(
   token: Token,
-  log: SubgraphDepositDataFragment | SubgraphRepaymentDataFragment | SubgraphBorrowDataFragment
-): DepositRecord | RepaymentRecord | BorrowRecord {
-  if ("account" in log) {
+  log: SubgraphWithdrawalRequestPropertiesFragment
+): WithdrawalRequestPartialRecord;
+export function parseMarketRecord(token: Token, log: MarketDataFragment): MarketRecord {
+  if (!hasTypeName(log)) {
+    throw Error(`Malformed market record: ${JSON.stringify(log, null, 2)}\nMissing __typename`);
+  }
+  if (isDeposit(log)) {
     const { account, assetAmount, ...rest } = log;
     return {
       ...rest,
       amount: token.getAmount(assetAmount),
       address: account.address
+    };
+  }
+  if (isDelinquencyStatusChanged(log)) {
+    const { liquidityCoverageRequired, totalAssets, ...rest } = log;
+    return {
+      ...rest,
+      liquidityCoverageRequired: token.getAmount(liquidityCoverageRequired),
+      totalAssets: token.getAmount(totalAssets)
+    };
+  }
+  if (isMarketClosed(log) || isAnnualInterestBipsUpdated(log)) {
+    return log;
+  }
+  if (isMaxTotalSupplyUpdated(log)) {
+    const { oldMaxTotalSupply, newMaxTotalSupply, ...rest } = log;
+    return {
+      ...rest,
+      oldMaxTotalSupply: token.getAmount(oldMaxTotalSupply),
+      newMaxTotalSupply: token.getAmount(newMaxTotalSupply)
+    };
+  }
+  if (isFeesCollected(log)) {
+    const { feesCollected, ...rest } = log;
+    return {
+      ...rest,
+      amount: token.getAmount(feesCollected)
+    };
+  }
+  if (isWithdrawalRequest(log)) {
+    const { scaledAmount, normalizedAmount, ...rest } = log;
+    return {
+      ...rest,
+      scaledAmount: BigNumber.from(scaledAmount),
+      normalizedAmount: token.getAmount(normalizedAmount)
     };
   }
   const { assetAmount, ...rest } = log;
