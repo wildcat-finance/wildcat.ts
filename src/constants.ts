531c92854ba8b21070d5cc8bed861803936cdede
Dave Coleman <dave@wildcat.finance>
2026-04-27 09:34:48 -0700

    release: v3.0.73-beta
    Signed-off-by: Dave Coleman <dave@wildcat.finance>

--- src/constants.ts
+++ src/constants.ts
@@ -724,7 +724,7 @@ export const getWrapperFactoryContract = (
 };
 
 export const SubgraphUrls = {
-  [SupportedChainId.Sepolia]: `https://api.goldsky.com/api/public/project_cmheai1ym00jyx7p27qn46qtm/subgraphs/sepolia/v2.0.32/gn`,
+  [SupportedChainId.Sepolia]: `https://api.goldsky.com/api/public/project_cmheai1ym00jyx7p27qn46qtm/subgraphs/sepolia/v2.0.33/gn`,
   [SupportedChainId.Mainnet]:
     "https://api.goldsky.com/api/public/project_cmheai1ym00jyx7p27qn46qtm/subgraphs/mainnet/v2.0.22/gn",
   [SupportedChainId.PlasmaTestnet]:
